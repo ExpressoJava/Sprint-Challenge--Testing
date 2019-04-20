82f498cf17dc4c67db3b4e9ad27b2966abe05dfa
Bao Pham <ryanvuepham@gmail.com>
2019-04-20 13:04:11 -0500

    tests for post

--- server.test.js
+++ server.test.js
@@ -23,4 +23,36 @@ describe('server.js', () => {
 			expect(Array.isArray(res.body)).toBeTruthy()
 		})
 	})
+
+	describe('POST /games', () => {
+		it('return list of games if added successfully', async () => {
+			const res = await request(server)
+				.post('/games')
+				.send({ title: 'Dragon Nest', genre: 'MMORPG', releaseYear: 2010 })
+			const expected = [
+				{ id: 1, title: 'Zelda', genre: 'Action-Adventure', releaseYear: 1986 },
+				{ id: 2, title: 'Arena of Valor', genre: 'MOBA', releaseYear: 2017 },
+				{ id: 3, title: 'Dragon Nest', genre: 'MMORPG', releaseYear: 2010 }
+			]
+			expect(res.body).toEqual(expected)
+		})
+
+		it('should return a status code of 422 if not enough information', async () => {
+			const res = await request(server)
+				.post('/games')
+				.send({
+					title: 'xyz'
+				})
+			expect(res.status).toBe(422)
+		})
+		it('return a status code of 405 if title already exists', async () => {
+			const res = await request(server)
+				.post('/games')
+				.send({
+					title: 'Zelda',
+					genre: 'Action-Adventure'
+				})
+			expect(res.status).toBe(405)
+		})
+	})
 })
